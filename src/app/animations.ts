2f9dcde6002ae141c9cd4bc67ff2aa732a6d6038
hararm <avarmenhg@yahoo.com>
2017-05-17 23:49:11 -0400

    commit

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,10 +1,37 @@
-import {state, style, trigger} from "@angular/animations";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
-export const showStateTrigger = trigger('showState',[
-  state('notShown',style ({
+export const showStateTrigger = trigger('showState', [
+  // transition('void => *', [
+  //   style({
+  //     opacity: 0
+  //   }),
+  //   animate(300, style({
+  //     opacity: 1
+  //   }))
+  // ]),
+  // transition('* => void', animate(300,style ({
+  //   opacity: 0
+  // })))
+  transition(':enter', [
+    style({
+      opacity: 0
+    }),
+    animate(300, style({
+      opacity: 1
+    }))
+  ]),
+  transition(':leave', animate(300,style ({
+    opacity: 0
+  })))
+]);
 
-  })),
-  state('shown', style({
-
-  }))
+export const animateStateTrigger = trigger('animateState', [
+  transition('*=>*', [
+    animate(400, style({
+      width: 0
+    })),
+    animate(400,style({
+      width: '*'
+    }))
+  ])
 ]);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+import {animateStateTrigger, showStateTrigger} from "./animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [showStateTrigger, animateStateTrigger]
 })
 export class AppComponent {
   isShown = false;
+  width = 400;
+  animate = false;
+  testResults = [];
 
   onToggleButtonClick() {
     this.isShown = !this.isShown;
     console.log('isShown= ', this.isShown);
+  }
+
+  onAddElement() {
+    this.testResults.push(Math.random());
   }
 }
